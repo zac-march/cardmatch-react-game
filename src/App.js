94ef2655c6292db5777a4b0ee2fd36224315fbbc
Zac <121649297+zac-march@users.noreply.github.com>
2023-05-10 20:13:30 +1000

    Add Header Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Game from "./components/Game";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Game />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Memory match</h1>
+      <h3>Try not to select the same card twice!</h3>
+    </div>
+  );
+};
+
+export default Header;
