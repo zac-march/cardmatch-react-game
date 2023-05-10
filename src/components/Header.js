3e27019992121d278ba833c8a2e65933205e2711
Zac <121649297+zac-march@users.noreply.github.com>
2023-05-11 08:08:06 +1000

    Edit subtitle

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React from "react";
 const Header = () => {
   return (
     <div className="header">
-      <h1>Memory match</h1>
-      <h3>Try not to select the same card twice!</h3>
+      <h1>Memory Match</h1>
+      <h3>Try not to select the same image twice!</h3>
     </div>
   );
 };
